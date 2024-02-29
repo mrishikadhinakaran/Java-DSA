6051d2319170f94777c62deb76b28a560bdff393
mrishika dhinakaran <140330194+mrishikadhinakaran@users.noreply.github.com>
2024-02-29 12:04:38 +0530

    function added

--- functions/Main.java
+++ functions/Main.java
@@ -1,26 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//this is a program to find the sum of 2 numbers in a function called by method.
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+         sum ();
+    }
+    static void sum(){
         Scanner in=new Scanner(System.in);
-
-        System.out.println("enter the first number:");
-        float first=in.nextFloat();
-
-        System.out.println("enter the second number:");
-        float second=in.nextFloat();
-
-        System.out.println("enter the third number:");
-        float third=in.nextFloat();
-        
-        float max=first;
-        if(second>max){
-            max=second;
-        }
-        if(third>max){
-            max=third;
-        }
-        System.out.println(max);
+        System.out.print("enter first number:");
+        int num1=in.nextInt();
+        System.out.print("enter second number:");
+        int num2=in.nextInt();
+        int sum=num1+num2;
+        System.out.println("the sum of the given number "+num1+" and "+num2+" is "+sum );
     }
 }
