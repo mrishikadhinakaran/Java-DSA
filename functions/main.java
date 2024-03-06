5c2befa4ce96b87aed00702f6d2e1ba18383eb6f
mrishika dhinakaran <140330194+mrishikadhinakaran@users.noreply.github.com>
2024-03-06 21:36:17 +0530

    function using shadowing

---
+++ functions/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        fun(34);
+    }
+    static void fun(int a){
+        System.out.println("firstone");
+        System.out.println(a);
+    }
+    static void fun(String b){
+        System.out.println("second one");
+        System.out.println(b);
+    }
+}
