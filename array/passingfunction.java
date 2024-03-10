f122d8018ad4367839c1f0857b8cef1db086416b
mrishika dhinakaran <140330194+mrishikadhinakaran@users.noreply.github.com>
2024-03-10 19:38:16 +0530

    2D array

--- array/passingfunction.java
+++ array/passingfunction.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class passingfunction {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] nums={2,3,4,5};
-        System.out.println(Arrays.toString(nums));
-        change(nums);
-        System.out.println(Arrays.toString(nums));
-    }
-    static void change(int[] arr){
-        arr[0]=99;
+        int[][] arr=new int[3][];
+        System.out.println(arr.length);
+
+
+
+        for (int row=0;row<arr.length; row++){
+            for(int col=0;col<arr[row].length; col++){
+                arr[row][col]=in.nextInt();
+            }
+        }
+        //output
+        for (int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                System.out.print(arr[row][col]+" ");
+            }
+            System.out.println();
+        }
     }
 }
