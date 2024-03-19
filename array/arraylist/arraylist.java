10af310d464dcf6a57193ecf3f11ffcf4a94d2e6
mrishika dhinakaran <140330194+mrishikadhinakaran@users.noreply.github.com>
2024-03-19 14:13:33 +0530

    swapping number in arraylist

--- array/arraylist/arraylist.java
+++ array/arraylist/arraylist.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Collection;
 
 public class arraylist {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-        for(int i=0;i<3;i++){
-            list.add(new ArrayList<>());
-        }
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                list.get(i).add(in.nextInt());
-            }
-        }
-        System.out.println(list);
+        int[] arr={1,2,3,4,5};
+        swap(arr, 1,4);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr,int index1,int index2){
+        int temp=arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+
     }
 }
