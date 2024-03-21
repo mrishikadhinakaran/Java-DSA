0e92bb313bdc3a5fd48e0aeb0ab2820b1aa4de23
mrishika dhinakaran <140330194+mrishikadhinakaran@users.noreply.github.com>
2024-03-21 20:28:15 +0530

    swapping elements in arraylist

--- array/arraylist/arraylist.java
+++ array/arraylist/arraylist.java
@@ -1,18 +1,19 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Collection;
 
 public class arraylist {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5};
-        swap(arr, 1,4);
+        int[] arr={1,23,4,5,56,57};
+        swap(arr, 0,4);
+
         System.out.println(Arrays.toString(arr));
     }
-    static void swap(int[] arr,int index1,int index2){
+    static void reverse(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+    }
+    static void swap(int[] arr, int index1, int index2){
         int temp=arr[index1];
         arr[index1]=arr[index2];
         arr[index2]=temp;
-
     }
 }
